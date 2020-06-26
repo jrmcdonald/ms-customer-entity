faac33492e0012a00bc642400e5e9b9b06f0023b
Jamie McDonald <jamie@qwyck.net>
2020-06-27 00:24:27 +0100

    feat: add location header to 201 response

--- customer-entity-endpoints/customer-entity-rest-api/src/main/java/com/jrmcdonald/customer/entity/api/doc/CustomerApi.java
+++ customer-entity-endpoints/customer-entity-rest-api/src/main/java/com/jrmcdonald/customer/entity/api/doc/CustomerApi.java
@@ -3,9 +3,12 @@ package com.jrmcdonald.customer.entity.api.doc;
 import com.jrmcdonald.customer.entity.api.model.CustomerRequest;
 import com.jrmcdonald.customer.entity.api.model.CustomerResponse;
 
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,7 +22,8 @@ public interface CustomerApi {
     @ApiResponse(responseCode = "500")
     ResponseEntity<CustomerResponse> getCustomer(String customerId);
 
-    @ApiResponse(responseCode = "201")
+
+    @ApiResponse(responseCode = "201", headers = @Header(name = HttpHeaders.LOCATION, description = "URI of the newly created customer account", schema = @Schema(type = "string")))
     @ApiResponse(responseCode = "401")
     @ApiResponse(responseCode = "403")
     @ApiResponse(responseCode = "409")
